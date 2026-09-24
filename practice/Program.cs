4c05c9b178a5b3fc4c51ba93ccda47b44578acfb
유우주 <uwooju98@gmail.com>
2026-09-24 20:39:25 +0900

    0924 2239 home

---
+++ practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+decimal haveGold;
+decimal priceJewlry;
+int buyjewlry;
+
+Console.Write("보유 골드: ");
+haveGold = decimal.Parse(Console.ReadLine());
+Console.Write("보석의 개당 가격: ");
+priceJewlry = decimal.Parse(Console.ReadLine());
+Console.Write("구매하고 싶은 보석 개수: ");
+buyjewlry = int.Parse(Console.ReadLine());
+
+
+int maximumQuantity = (int)(haveGold / priceJewlry);
+int realQuantity = Math.Min(maximumQuantity, buyjewlry);
+
+decimal usingGold = priceJewlry * realQuantity;
+decimal remainGold = haveGold - usingGold;
+
+
+Console.WriteLine($"구매 가능한 최대 개수: {maximumQuantity}");
+Console.WriteLine($"실제 구매 개수: {realQuantity}");
+Console.WriteLine($"사용한 골드: {usingGold}");
+Console.WriteLine($"남은 골드: {remainGold}");
+
---
+++ practice응용/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+decimal haveGold;
+decimal secondGold;
+decimal priceJewlry;
+int buyJewlry;
+
+
+Console.Write("보석의 개당 가격:");
+priceJewlry = decimal.Parse(Console.ReadLine());
+Console.Write("구매하고 싶은 보석 개수:");
+buyJewlry = int.Parse(Console.ReadLine());
+Console.Write("첫 번째 충전 골드:");
+haveGold = decimal.Parse(Console.ReadLine());
+Console.Write("두 번째 충전 골드:");
+secondGold = decimal.Parse(Console.ReadLine());
+
+
+int firstQuantity = (int)Math.Min(haveGold/priceJewlry,buyJewlry); //첫 구매 최대 수량
+
+Console.WriteLine($"첫 번째 구매 개수: {firstQuantity}");
+
+
+haveGold = haveGold - (priceJewlry * firstQuantity); // 첫 구매후 잔액
+Console.WriteLine($"첫 번째 구매 후 잔액: {haveGold}");
+buyJewlry = buyJewlry - firstQuantity; // 첫 구매후 사고싶은 남은 수량
+
+
+haveGold = haveGold + secondGold; // 두 번째 골드 충전
+
+
+int secondQuantity = (int)Math.Min(haveGold/priceJewlry,buyJewlry);//(최대개수/사고싶은양)
+Console.WriteLine($"두 번째 구매 개수: {secondQuantity}");
+
+
+int totalQuantity = firstQuantity + secondQuantity;
+Console.WriteLine($"총 구매 개수: {totalQuantity}");
+
+haveGold = haveGold - priceJewlry * secondQuantity;
+Console.WriteLine($"최종 잔액: {haveGold}");
+IAsyncDisposable
